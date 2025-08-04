2886d5de5f94e43fa6c35617931baf64754aeb09
jbzala <jbzala@HW00924.NCODECORP.IN>
2025-08-04 10:26:28 +0530

    Push

---
+++ ExceptionFandlingAndStringClass/src/trstcases/HandleException.java
@@ -0,0 +1,31 @@
+package trstcases;
+
+public class HandleException {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("Open Browser");
+		System.out.println("Meximize the Window");
+		System.out.println("Loading the URL");
+		System.out.println("Signin ");
+		System.out.println("Searching the product");
+		
+		
+		
+		try
+		{
+			int count = 10/0;
+		}
+		catch(Exception e)
+		{
+			System.out.println("Error Occurred");
+		}
+		
+		System.out.println("Sign Out");
+		System.out.println("Close the borser");
+		
+		
+	}
+
+}
